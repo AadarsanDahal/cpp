89e716bb39bfb57c7532eff61653bfcc815ec9e9
AadarsanDahal <aadarsandahal@gmail.com>
2025-05-10 00:20:36 +0545

    made banking system's basic and grade's app

---
+++ Banking.cpp
@@ -0,0 +1,11 @@
+
+// // 6. **Simple Menu-Driven Bank System**
+
+// make menue//    
+
+// * Deposit, ask amt to put in
+// withdraw, minus from the var
+// check balance. print the var
+// //    *Do you want to calculate intrest of yours    ----> Call function to calculate SI *   
+
+// //    * Use **functions**, **pass by reference**, and **control structures**.
