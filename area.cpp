a7e40aa81cd931a8d455103fa8450019eec00c1e
AadarsanDahal <aadarsandahal@gmail.com>
2025-05-13 08:36:43 +0545

    area.cpp live

---
+++ area.cpp
@@ -0,0 +1,17 @@
+//Fiding the area of a circle 
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+float radius,area;
+
+
+int main (){
+cout << "Enter the radius of the circle: \n ";
+cin >> radius;
+area = 3.14*pow(radius,2);
+cout << "The area of the circle is: \n" << area ;
+
+
+    return 0;
+}
