b71a440fd5aa1f8f835e5f8622e197e78caee611
AadarsanDahal <aadarsandahal@gmail.com>
2025-05-27 18:33:40 +0545

    jus commit

--- user_num_top_10.cpp
+++ user_num_top_10.cpp
@@ -7,11 +7,82 @@ int main() {
      
 
 
-    for (int i = 10; i >=0;  i=i-2)
-    {
-     cout << i << endl;
-    } 
+    // for (int i = 10; i >=0;  i=i-2)
+    // {
+    //  cout << i << endl;
+    // } 
     
+
+  
+//     int a=0;
+//     bool running_loop = true;
+
+// while (running_loop)
+// {
+// cout << "enter a num" << endl;
+// cin >>a;
+
+// if (a <0)
+// {
+//    cout << "neg num" <<endl;
+//    running_loop = false;
+//    break;
+// }
+
+// else (a >= 0);
+// {
+//     cout << "positive num" << endl;
+   
+// }
+
+float n,n2;
+
+
+bool condition = true;
+
+// do
+// {
+//    cout << "enter a num" << endl;
+//    cin >> n;
+
+
+
+//    if (n<0.00){
+//     cout << "negative num" << endl;
+//     condition = false;
+//     break;
+//    }
+
+//    else
+//    {
+//     cout << "Positive num " << endl;
+//    }
+   
+// } while (condition);
+do
+{
+   cout << "enter a num" << endl;
+   cin >> n;
+
+
+
+   if (n<0.00){
+    cout << "negative num" << endl;
+    condition = false;
+    break;
+   }
+
+   else
+   {
+    cout << "Positive num " << endl;
+   }
+   
+} while (condition);
+
+
+
+
+
     return 0;
 }
 
